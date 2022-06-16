9baeb40fd882217156dad6fd00c8e47eeae54129
apple </Kvadrat2020/>
2022-06-16 14:00:09 -0400

    zaregistriroval apps in webapp to file settings

--- homework43/settings.py
+++ homework43/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp_hw.apps.WebappHwConfig',
+    'webapp_hw',
 ]
 
 MIDDLEWARE = [
